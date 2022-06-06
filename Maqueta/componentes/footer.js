1bb04c7526d4117e089b444b09bb86f64d40fb19
claudia-2001 <91056896+claudia-2001@users.noreply.github.com>
2022-06-06 10:29:30 +0200

    Add files via upload

---
+++ Maqueta/componentes/footer.js
@@ -0,0 +1,70 @@
+const footerTemplate = document.createElement('template');
+
+footerTemplate.innerHTML = `
+  <style>
+    footer {
+      height: 40px;
+      padding: 0 10px;
+      list-style: none;
+      display: flex;
+      flex-shrink: 0;
+      justify-content: space-between;
+      align-items: center;
+      background-color: red;
+    }
+
+    ul {
+      padding: 0;
+    }
+    
+    ul li {
+      list-style: none;
+      display: inline;
+    }
+    
+    a {
+      margin: 0 15px;
+      color: inherit;
+      text-decoration: none;
+    }
+    
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #333;
+    }
+    
+    .social-row {
+      font-size: 20px;
+    }
+    
+    .social-row li a {
+      margin: 0 15px;
+    }
+  </style>
+  <footer>
+    <ul>
+      <li><a href="Inicio.html">Inicio</a></li>
+      <li><a href="Empresas.html">Empresas</a></li>
+      <li><a href="Informes.html">Informes</a></li>
+    </ul>
+  </footer>
+`;
+
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const fontAwesome = document.querySelector('link[href*="font-awesome"]');
+        const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+        if (fontAwesome) {
+            shadowRoot.appendChild(fontAwesome.cloneNode());
+        }
+
+        shadowRoot.appendChild(footerTemplate.content);
+    }
+}
+
+customElements.define('footer-component', Footer);
